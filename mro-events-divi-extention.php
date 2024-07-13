261a7864843d89965bb2e6943f3c9ca62e2f3e15
Ratan Mia <ratan.kawasaki@gmail.com>
2024-07-13 23:30:31 +0600

    added pagination

--- mro-events-divi-extention.php
+++ mro-events-divi-extention.php
@@ -52,133 +52,8 @@ if (!function_exists('mro_initialize_extension')) :
 endif;
 
 
+// Enqueue scripts
 
-// add bootstrap css and js
-
-
-function mro_events_divi_extention_bootstrap_scripts()
-{
-    wp_enqueue_style('bootstrap-css', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css');
-    wp_enqueue_script('bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js', array('jquery'), null, true);
-}
-add_action('wp_enqueue_scripts', 'mro_events_divi_extention_bootstrap_scripts');
-
-
-// shortcode for filtering post
-
-
-function custom_post_filter_shortcode()
-{
-    ob_start();
-?>
-    <form method="GET" id="filter-form" class="d-flex flex-wrap align-items-center mb-4">
-        <div class="form-group mr-3 mb-2">
-            <label for="author" class="mr-2">Author:</label>
-            <select name="author" id="author" class="form-control">
-                <option value="">Select Author</option>
-                <?php
-                $authors = get_users(array('who' => 'authors'));
-                foreach ($authors as $author) {
-                    echo '<option value="' . $author->ID . '">' . $author->display_name . '</option>';
-                }
-                ?>
-            </select>
-        </div>
-
-        <div class="form-group mr-3 mb-2">
-            <label for="category" class="mr-2">Category:</label>
-            <select name="category" id="category" class="form-control">
-                <option value="">Select Category</option>
-                <?php
-                $categories = get_categories();
-                foreach ($categories as $category) {
-                    echo '<option value="' . $category->term_id . '">' . $category->name . '</option>';
-                }
-                ?>
-            </select>
-        </div>
-
-        <div class="form-group mr-3 mb-2">
-            <label for="tags" class="mr-2">Tags:</label>
-            <select name="tags" id="tags" class="form-control">
-                <option value="">Select Tags</option>
-                <?php
-                $tags = get_tags();
-                foreach ($tags as $tag) {
-                    echo '<option value="' . $tag->term_id . '">' . $tag->name . '</option>';
-                }
-                ?>
-            </select>
-        </div>
-
-        <div class="form-group mr-3 mb-2">
-            <label for="search" class="mr-2">Search:</label>
-            <input type="text" name="search" id="search" class="form-control" placeholder="Search...">
-        </div>
-
-        <button type="submit" class="btn btn-primary mb-2">Filter</button>
-    </form>
-
-    <?php
-    $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-
-    $author = isset($_GET['author']) ? sanitize_text_field($_GET['author']) : '';
-    $category = isset($_GET['category']) ? sanitize_text_field($_GET['category']) : '';
-    $tags = isset($_GET['tags']) ? sanitize_text_field($_GET['tags']) : '';
-    $search = isset($_GET['search']) ? sanitize_text_field($_GET['search']) : '';
-
-    $args = array(
-        'post_type' => 'post',
-        'posts_per_page' => 6, // Adjust the number of posts per page as needed
-        'paged' => $paged,
-        'author' => $author,
-        'category' => $category,
-        'tag__in' => $tags ? array($tags) : array(),
-        's' => $search
-    );
-
-    $query = new WP_Query($args);
-    if ($query->have_posts()) :
-        echo '<div class="row">';
-        while ($query->have_posts()) : $query->the_post();
-    ?>
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title"><?php the_title(); ?></h5>
-                        <p class="card-text"><?php the_excerpt(); ?></p>
-                        <a href="<?php the_permalink(); ?>" class="btn btn-primary">Read More</a>
-                    </div>
-                </div>
-            </div>
-        <?php
-        endwhile;
-        echo '</div>';
-        ?>
-        <div class="pagination">
-            <?php
-            echo paginate_links(array(
-                'total' => $query->max_num_pages,
-                'current' => $paged,
-                'prev_text' => __('« Prev'),
-                'next_text' => __('Next »'),
-            ));
-            ?>
-        </div>
-        <?php
-    else :
-        echo '<p>No posts found</p>';
-    endif;
-
-    wp_reset_postdata();
-    return ob_get_clean();
-}
-
-add_shortcode('custom_post_filter', 'custom_post_filter_shortcode');
-
-
-
-// Enqueue Bootstrap and custom JavaScript
 function enqueue_custom_scripts()
 {
     wp_enqueue_style('bootstrap-css', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css');
@@ -192,7 +67,10 @@ function enqueue_custom_scripts()
 }
 add_action('wp_enqueue_scripts', 'enqueue_custom_scripts');
 
-// AJAX handler function
+
+
+// Add shortcode to filter the posts
+
 function filter_posts()
 {
     check_ajax_referer('afp_nonce', 'afp_nonce');
@@ -217,7 +95,7 @@ function filter_posts()
     if ($query->have_posts()) :
         echo '<div class="row">';
         while ($query->have_posts()) : $query->the_post();
-        ?>
+?>
             <div class="col-md-4 mb-4">
                 <div class="card">
                     <div class="card-body">
@@ -253,8 +131,10 @@ function filter_posts()
 add_action('wp_ajax_filter_posts', 'filter_posts');
 add_action('wp_ajax_nopriv_filter_posts', 'filter_posts');
 
-// Shortcode function
-function custom_post_filter_ajax_shortcode()
+
+
+
+function custom_post_filter_shortcode()
 {
     ob_start();
     ?>
@@ -306,16 +186,62 @@ function custom_post_filter_ajax_shortcode()
         <button type="submit" class="btn btn-primary mb-2">Filter</button>
     </form>
 
-    <div id="response"></div>
+    <div id="response">
+        <?php
+        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+        $args = array(
+            'post_type' => 'post',
+            'posts_per_page' => 6,
+            'paged' => $paged,
+        );
+
+        $query = new WP_Query($args);
+        if ($query->have_posts()) :
+            echo '<div class="row">';
+            while ($query->have_posts()) : $query->the_post();
+        ?>
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php the_title(); ?></h5>
+                            <p class="card-text"><?php the_excerpt(); ?></p>
+                            <a href="<?php the_permalink(); ?>" class="btn btn-primary">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            <?php
+            endwhile;
+            echo '</div>';
+            ?>
+            <div class="pagination">
+                <?php
+                echo paginate_links(array(
+                    'total' => $query->max_num_pages,
+                    'current' => $paged,
+                    'prev_text' => __('« Prev'),
+                    'next_text' => __('Next »'),
+                    'format' => '?paged=%#%',
+                ));
+                ?>
+            </div>
+        <?php
+        else :
+            echo '<p>No posts found</p>';
+        endif;
+
+        wp_reset_postdata();
+        ?>
+    </div>
 
 <?php
     return ob_get_clean();
 }
 
-add_shortcode('custom_post_filter_ajax', 'custom_post_filter_ajax_shortcode');
+add_shortcode('custom_post_filter', 'custom_post_filter_shortcode');
 
 
-// inline post filter script
+
+// Add AJAX actions to filter the post
 
 function inline_ajax_filter_script()
 {
